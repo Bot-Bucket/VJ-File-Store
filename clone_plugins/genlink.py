8658f6ec505dc44c92d9efc80e32ca22562ac094
Bot-Bucket <163278950+Bot-Bucket@users.noreply.github.com>
2024-05-11 13:16:39 +0530

    Update genlink.py

--- clone_plugins/genlink.py
+++ clone_plugins/genlink.py
@@ -1,31 +1,21 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import re
+import base64
 from pyrogram import filters, Client, enums
 from plugins.database import unpack_new_file_id
 from clone_plugins.users_api import get_user, get_short_link
-import base64
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-@Client.on_message(filters.command(['link', 'plink']))
+@Client.on_message(filters.command(['link', 'plink', 'batch']))
 async def gen_link_s(client: Client, message):
     replied = message.reply_to_message
     if not replied:
         return await message.reply('Reply to a message to get a shareable link.')
+    
     file_type = replied.media
     if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
         return await message.reply("Reply to a supported media")
+    
     if message.has_protected_content:
         return await message.reply("okDa")
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
     
     file_id, ref = unpack_new_file_id((getattr(replied, file_type.value)).file_id)
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
@@ -33,15 +23,19 @@ async def gen_link_s(client: Client, message):
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
     user_id = message.from_user.id
     user = await get_user(user_id)
-    # Get the bot's username
+    
+    # Generate individual share links
     bot_username = (await client.get_me()).username
     share_link = f"https://t.me/{bot_username}?start={outstr}"
-    await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
     short_link = await get_short_link(user, share_link)
-    await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
     
+    # Handle batch command
+    if message.command[0] == 'batch':
+        # Generate batch link
+        batch_link = f"https://t.me/{bot_username}?start=batch_{outstr}"
+        batch_short_link = await get_short_link(user, batch_link)
+        await message.reply(f"<b>Batch Link:\n{batch_short_link}</b>")
+    else:
+        # Reply with individual share link
+        await message.reply(f"<b>Original Link:\n{share_link}</b>")
+        await message.reply(f"<b>Short Link:\n{short_link}</b>")
